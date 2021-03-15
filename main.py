33d9a56233c22e31e1a9b635be2e5a36967235e8
Jorge Zaldívar <jorge.zaldivar@gmail.com>
2021-03-15 00:59:10 -0600

    Roberto, Alejandra y Juan Pablo Valdez

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
-"""
-Inserta el encabezado aquí y escribe tu código abajo
-"""
+# Año bisiesto
+# Equipo 2: Roberto, Alejandra y Juan Pablo Valdez
+# Fecha: 25/02/2021
 
-# Declaraciones
-CONSTANTE = valor
+#Entradas
+a = int(input("Ingrese el año a verificar: "))
 
-# Entradas
-entrada = input()
+#Proceso
+if a % 4 != 0: 
+	print("El año ingresado no es bisiesto")
+elif a % 4 == 0 and a % 100 != 0: 
+	print("El año ingresado sí es bisiesto")
+elif a % 4 == 0 and a % 100 == 0 and a % 400 != 0:
+	print("El año ingresado sí no es bisiesto")
+elif a % 4 == 0 and a % 100 == 0 and a % 400 == 0: 
+	print("El año ingresado sí es bisiesto")
 
-# Proceso
-
-
-# Salidas
-print(salida)
